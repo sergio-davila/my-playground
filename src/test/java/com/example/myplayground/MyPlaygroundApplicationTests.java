3f0cb646b219c08fb0e66251f9b4ee0ecbdb289f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-07-20 23:06:21 -0500

    feat: added gitignore

---
+++ src/test/java/com/example/myplayground/MyPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
