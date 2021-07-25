5d0ace2c7f211f88f01305524d597364244b71cf
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-07-24 19:33:40 -0500

    feat: finished loops1

---
+++ src/main/java/com/example/myplayground/hackerrank/Loop1.java
@@ -0,0 +1,13 @@
+package com.example.myplayground.hackerrank;
+
+public class Loop1 {
+    public static void main(String[] args) {
+
+        int N = 2;
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.printf("%d x %d = %d\n", N, i, result);
+        }
+
+    }
+}
