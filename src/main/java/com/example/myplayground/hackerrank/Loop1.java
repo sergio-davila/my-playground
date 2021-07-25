9e2b6a8116bfc1955cb39374289d6f79ecad5b9f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-07-24 20:30:05 -0500

    feat: console printing is giving me trouble

--- src/main/java/com/example/myplayground/hackerrank/Loop1.java
+++ src/main/java/com/example/myplayground/hackerrank/Loop1.java
@@ -1,5 +1,7 @@
 package com.example.myplayground.hackerrank;
 
+import java.util.Scanner;
+
 public class Loop1 {
     public static void main(String[] args) {
 
@@ -7,6 +9,25 @@ public class Loop1 {
         for (int i = 1; i <= 10; i++) {
             int result = N * i;
             System.out.printf("%d x %d = %d\n", N, i, result);
+        }
+
+        // still working on this one, havent done console prints in a while
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            if(a + b <= 2) {
+                System.out.print((a + (b * 1)) + " ");
+            } else {
+                System.out.print((a + (b * 1) + (a+b) * (a+b)) + " ");
+            }
+
+            // else if((a + (b * 1) + (a+b) * (a+b))) {
+            //     System.out.print((a + (b * 1) + (a+b) * (a+b)) + " ");
+            // }
+            // System.out.print();
         }
 
     }
