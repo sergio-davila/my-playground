1cc80274a7868b474da417355cd279c4ea4c002d
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-07-25 23:48:33 -0500

    feat: learned limits of certain datatypes

---
+++ src/main/java/com/example/myplayground/hackerrank/Datatypes.java
@@ -0,0 +1,25 @@
+package com.example.myplayground.hackerrank;
+
+import java.util.Scanner;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            long x=sc.nextLong();
+            System.out.println(x+" can be fitted in:");
+            if(x>=-128 && x<=127)System.out.println("* byte");
+            if(x>=-32768 && x<=32767)System.out.println("* short");
+            if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+            // if(x>=-9223372036854775808 && x<=9223372036854775807)System.out.println("* long");
+            System.out.println("* long");
+            //Complete the code
+        }
+        catch(Exception e)
+        {
+            System.out.println(sc.next()+" can't be fitted anywhere.");
+        }
+    }
+
+}
