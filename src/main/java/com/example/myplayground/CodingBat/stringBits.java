72f5adddb0c20375ed9fd10d8742514a223ca2eb
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-01 18:49:39 -0500

    feat: further substring studies

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -92,6 +92,17 @@ public class stringBits {
         return count;
     }
 
+    public String stringX(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            // Only append the char if it is not the "x" case
+            if (!(i > 0 && i < (str.length() - 1) && str.substring(i, i + 1).equals("x"))) {
+                result = result + str.substring(i, i + 1); // Could use str.charAt(i) here
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
 
