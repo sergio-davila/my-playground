3d9df2c244ec10a1a27e3d596e28dce13002c866
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-07-31 23:24:59 -0500

    feat: solved a codingbat problem

--- src/main/java/com/example/myplayground/CodingBat/stringTimes.java
+++ src/main/java/com/example/myplayground/CodingBat/stringTimes.java
@@ -1,11 +1,11 @@
-package com.example.myplayground.CodingBat;
-
-public class stringTimes {
-    public String stringTimes(String str, int n) {
-        String result = "";
-        for (int i=0; i<n; i++) {
-            result = result + str;  // could use += here
-        }
-        return result;
-    }
-}
+//package com.example.myplayground.CodingBat;
+//
+//public class stringTimes {
+//    public String stringTimes(String str, int n) {
+//        String result = "";
+//        for (int i=0; i<n; i++) {
+//            result = result + str;  // could use += here
+//        }
+//        return result;
+//    }
+//}
