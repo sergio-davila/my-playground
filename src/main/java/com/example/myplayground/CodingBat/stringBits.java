4d515f7dbbf1402042bd93bc334705d50b6e5eca
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-01 18:28:04 -0500

    feat: figuring out array problems in codingbat

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -65,6 +65,16 @@ public class stringBits {
         return false;
     }
 
+    public boolean array123(int[] nums) {
+        // Note: iterate < length-2, so can use i+1 and i+2 in the loop
+        for (int i=0; i < (nums.length-2); i++) {
+            if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
 
