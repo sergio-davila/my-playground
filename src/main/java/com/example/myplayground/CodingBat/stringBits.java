a5e3eee48a16031bed518134d01f5960d8595faf
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-01 17:31:50 -0500

    feat: solved stringsplosion

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -10,4 +10,19 @@ public class stringBits {
         }
         return result;
     }
+
+    public static String stringSplosion(String str) {
+        String result = "";
+        for(int i = 0; i < str.length(); i++) {
+            result += str.substring(0, i+1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(stringSplosion("hello"));
+
+    }
 }
