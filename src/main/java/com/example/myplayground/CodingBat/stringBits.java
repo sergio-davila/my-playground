e36356c6603a8152bad9b275882ded8c8be9d8b1
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-02 18:05:50 -0500

    feat: worked on more substring problems

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -103,6 +103,22 @@ public class stringBits {
         return result;
     }
 
+    public String altPairs(String str) {
+        String result = "";
+
+        // Run i by 4 to hit 0, 4, 8, ...
+        for (int i = 0; i < str.length(); i += 4) {
+            // Append the chars between i and i+2
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
 
 
