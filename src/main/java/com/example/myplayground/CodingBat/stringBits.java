482945e586cfde24aadf1db05b9132e630ef61d7
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-03 22:29:02 -0500

    feat: finished another array problem

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -147,6 +147,16 @@ public class stringBits {
         return result;
     }
 
+    public boolean noTriples(int[] nums) {
+
+        for(int i = 0; i < nums.length - 2; i++){
+            if(nums[i + 1] == nums[i] && nums[i + 2] == nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
 
