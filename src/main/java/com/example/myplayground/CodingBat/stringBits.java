65914cfe5b9a77523c4391a1e43d784f70a3c09a
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-03 22:22:02 -0500

    feat: finished a codingbat problem about array iteration

--- src/main/java/com/example/myplayground/CodingBat/stringBits.java
+++ src/main/java/com/example/myplayground/CodingBat/stringBits.java
@@ -134,6 +134,19 @@ public class stringBits {
         return result;
     }
 
+    public int array667(int[] nums) {
+
+        int result = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == 6) {
+                if(nums[i + 1] == 6 || nums[i + 1] == 7) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
 
