2daa5d5a305e337e33b82ce1c21964ed94467328
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-07 21:58:48 -0500

    feat: solved more20 problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -92,5 +92,11 @@ public class Logic1 {
         return false;
     }
 
+    public boolean more20(int n) {
+        if(n % 20 == 1 || n % 20 == 2) {
+            return true;
+        }
+        return false;
+    }
 
 }
