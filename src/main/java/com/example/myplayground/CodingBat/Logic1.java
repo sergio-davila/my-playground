41a70312f223608006d215ab6a277204e26bc287
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-07 22:01:37 -0500

    feat: solved old35 problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -99,4 +99,11 @@ public class Logic1 {
         return false;
     }
 
+    public boolean old35(int n) {
+        if((n % 3 == 0 || n % 5 == 0) && !(n % 3 == 0 && n % 5 == 0)) {
+            return true;
+        }
+        return false;
+    }
+
 }
