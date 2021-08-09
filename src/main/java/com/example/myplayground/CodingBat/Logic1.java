eed8a6e4806f161c0d7b676d5d66796f5298873d
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-08 22:47:05 -0500

    feat: solved less20 codingbat problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -106,4 +106,12 @@ public class Logic1 {
         return false;
     }
 
+    public boolean less20(int n) {
+        if(n % 20 == 19 || n % 20 == 18) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
