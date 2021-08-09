0ad192153fa49dd3b990ef36db8ced8cbbccccda
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-08 23:32:21 -0500

    feat: solved teaPArty problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -137,4 +137,16 @@ public class Logic1 {
         }
         return true;
     }
+
+    public int teaParty(int tea, int candy) {
+        if(tea < 5 || candy < 5) {
+            return 0;
+        } else if(tea >= candy * 2 || candy >= tea * 2) {
+            return 2;
+        } else if(tea >= 5 || candy >= 5) {
+            return 1;
+        }
+        return 1;
+    }
+
 }
