ccaf3a3be51156da2defd1555d0e585e3d82887d
sergio-davila <sergio.alexander.davila15@gmail.com>
2021-08-11 23:56:00 -0500

    feat: solved twoAsOne problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -189,4 +189,11 @@ public class Logic1 {
         return n + "!";
     }
 
+    public boolean twoAsOne(int a, int b, int c) {
+        if(a + b == c || a + c == b || b + c == a) {
+            return true;
+        }
+        return false;
+    }
+
 }
