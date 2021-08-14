c863bb753b25af3ac82dddc0eb5717c02a455c16
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-14 00:09:36 -0500

    feat: solved withoutDoubles

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -221,4 +221,17 @@ public class Logic1 {
         return false;
     }
 
+    public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        if(noDoubles && die1 == die2) {
+            if(die2 == 6) {
+                die2 = 1;
+            } else {
+                die2++;
+            }
+            return die1 + die2;
+        }
+        return die1 + die2;
+    }
+
+
 }
