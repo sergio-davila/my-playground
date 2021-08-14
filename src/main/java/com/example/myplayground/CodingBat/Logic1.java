068af0c1e2a23cb5bb82ef0d9a500c69c07f0574
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-14 00:16:59 -0500

    feat: finished maxMod5 problem

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -233,5 +233,23 @@ public class Logic1 {
         return die1 + die2;
     }
 
+    public int maxMod5(int a, int b) {
+        if(a == b) {
+            return 0;
+        } else if(a % 5 == b % 5) {
+            if(a < b) {
+                return a;
+            } else {
+                return b;
+            }
+        } else {
+            if(a > b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+    }
+
 
 }
