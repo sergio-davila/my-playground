1ca3ae41ed21d568bff10d717fdc903af6ae6c37
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-13 22:21:24 -0500

    feat: solved lastdigit

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -210,4 +210,8 @@ public class Logic1 {
         return false;
     }
 
+    public boolean lastDigit(int a, int b, int c) {
+        return (a % 10 == b % 10) || (a % 10 == c % 10) || (b % 10 == c % 10);
+    }
+
 }
