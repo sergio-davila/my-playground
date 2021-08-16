b1e099a324fc965b6b639cf0a9fb889b9a7dccbc
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-15 20:46:59 -0500

    feat: solved blueticket

--- src/main/java/com/example/myplayground/CodingBat/Logic1.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic1.java
@@ -283,5 +283,14 @@ public class Logic1 {
         }
     }
 
+    public int blueTicket(int a, int b, int c) {
+        if(a + b == 10 || a + c == 10 || b + c == 10) {
+            return 10;
+        } else if(a + b == (10 + a + c) || a + b == (10 + b + c)) {
+            return 5;
+        } else {
+            return 0;
+        }
+    }
 
 }
