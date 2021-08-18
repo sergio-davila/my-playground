ad4e214ac402e90ac5ca441b7c12611daa5d8db4
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-17 22:08:09 -0500

    feat: worked on noteensum

--- src/main/java/com/example/myplayground/CodingBat/Logic2.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic2.java
@@ -49,4 +49,15 @@ public class Logic2 {
         }
         return a + b + c;
     }
+
+    public int noTeenSum(int a, int b, int c) {
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
+    }
+
+    public int fixTeen(int n) {
+        if(13 <= n && n <= 19 && n != 15 && n != 16)
+            return 0;
+
+        return n;
+    }
 }
