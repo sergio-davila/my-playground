930fcb5f91492f19dbbb9e3250667dedde001196
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-17 21:58:38 -0500

    feat: worked on luckysum

--- src/main/java/com/example/myplayground/CodingBat/Logic2.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic2.java
@@ -29,4 +29,24 @@ public class Logic2 {
         }
         return a + b + c;
     }
+
+    public int luckySum(int a, int b, int c) {
+        int aNew = a;
+        int bNew = b;
+        int cNew = c;
+
+        if(aNew == 13) {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+        if(bNew == 13) {
+            b = 0;
+            c = 0;
+        }
+        if(cNew == 13) {
+            c = 0;
+        }
+        return a + b + c;
+    }
 }
