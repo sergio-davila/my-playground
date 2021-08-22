2c9f964f8707a8ae7d1d4978d9f525504c3443eb
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-22 18:39:38 -0500

    feat: solved java static initializer block

--- src/main/java/com/example/myplayground/hackerrank/Java.java
+++ src/main/java/com/example/myplayground/hackerrank/Java.java
@@ -3,8 +3,29 @@ package com.example.myplayground.hackerrank;
 import java.util.Scanner;
 
 public class Java {
+
+    // java static initializer block
+    static Scanner input = new Scanner(System.in);
+    static boolean flag = true;
+    static int B = input.nextInt();
+    static int H = input.nextInt();
+
+    static{
+        try{
+            if(B <= 0 || H <= 0){
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
+        // java output formatting
         System.out.println("================================");
         for(int i=0;i<3;i++)
         {
@@ -14,9 +35,14 @@ public class Java {
         }
         System.out.println("================================");
 
-
+        // java end of file
         for(int i = 1; sc.hasNext()== true; i++){
             System.out.println(i + " " + sc.nextLine());
+        }
+
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
         }
     }
 }
