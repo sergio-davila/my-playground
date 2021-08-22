c985e8845d4b0e279563d71239b9f3d5563b2bf7
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-21 21:02:55 -0500

    feat: solved firstLast6

--- src/main/java/com/example/myplayground/CodingBat/Logic2.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic2.java
@@ -166,7 +166,12 @@ public class Logic2 {
         return -1;
     }
 
-
+    public boolean firstLast6(int[] nums) {
+        if(nums[0] == 6 || nums[nums.length - 1] == 6) {
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         System.out.println(12 / 10);
