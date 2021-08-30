fca16b30d5aa4012c07d4779b6183ec84742aa44
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-30 05:58:07 -0500

    Feat: solved java method overriding

--- src/main/java/com/example/myplayground/hackerrank/OOP.java
+++ src/main/java/com/example/myplayground/hackerrank/OOP.java
@@ -8,6 +8,28 @@ import java.util.regex.*;
 public class OOP {
 }
 
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
+
 interface AdvancedArithmetic{
     int divisor_sum(int n);
 }
