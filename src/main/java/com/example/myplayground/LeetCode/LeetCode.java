19e29d20eb6652fcada649c686df8bbd852f1854
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-04 20:34:17 -0500

    feat: worked/studied about the three sum problem

--- src/main/java/com/example/myplayground/LeetCode/LeetCode.java
+++ src/main/java/com/example/myplayground/LeetCode/LeetCode.java
@@ -1,5 +1,9 @@
 package com.example.myplayground.LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LeetCode {
 
     private static int[] findTwoSum_BruteForce(int[] nums, int target) {
@@ -11,5 +15,40 @@ public class LeetCode {
             }
         }
         return new int[] {};
+    }
+
+    private static List<Integer[]> findThreeSum_BruteForce(int[] nums, int target) {
+        List<Integer[]> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == target) {
+                        result.add(new Integer[] { nums[i], nums[j], nums[k] });
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private static List<Integer[]> findThreeSum_Sorting(int[] nums, int target) {
+        List<Integer[]> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == target) {
+                    result.add(new Integer[] { nums[i], nums[left], nums[right] });
+                    left++;
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 }
