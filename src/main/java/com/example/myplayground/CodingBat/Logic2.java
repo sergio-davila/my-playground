c05d7d2b0c5acabbe991e70845c8eb36e3c3aa01
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-09 23:55:21 -0500

    feat: solved matchup

--- src/main/java/com/example/myplayground/CodingBat/Logic2.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic2.java
@@ -386,11 +386,17 @@ public class Logic2 {
                 fouriterator += 1;
             }
         }
-        if((twoiterator == 1 && fouriterator == 1) || (twoiterator == 0 && fouriterator == 0)) {
-            return false;
-        } else {
-            return true;
+        return (twoiterator != 1 || fouriterator != 1) && (twoiterator != 0 || fouriterator != 0);
+    }
+
+    public int matchUp(int[] nums1, int[] nums2) {
+        int iterator = 0;
+        for(int i = 0; i < nums1.length; i++) {
+            if(Math.abs(nums1[i] - nums2[i]) <= 2 && nums1[i] != nums2[i])
+                iterator++;
         }
+
+        return iterator;
     }
 
     public static void main(String[] args) {
