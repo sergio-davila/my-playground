1613f7a931b05e094582c1939937deeea1b5df6f
Sergio Alexander Davila <78042215+sergio-davila@users.noreply.github.com>
2021-09-22 23:40:24 -0500

    worked on and solved modThree from codingbat

--- src/main/java/com/example/myplayground/CodingBat/Logic2.java
+++ src/main/java/com/example/myplayground/CodingBat/Logic2.java
@@ -420,6 +420,18 @@ public class Logic2 {
         }
         return false;
     }
+    
+    public boolean modThree(int[] nums) {
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+                return true;
+            }
+            if(nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         System.out.println(12 / 10);
